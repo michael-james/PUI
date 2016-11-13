89621ecadfd303a976c2d456a048474b59eb69d3
Michael James <thelivingpc@gmail.com>
2016-11-13 00:36:16 -0500

    MVP

--- hw5/js/myscript.js
+++ hw5/js/myscript.js
@@ -1,8 +1,25 @@
 $(".cat").on("click", function() {
-	alert("Test");
+	var name = $(this).find("span");
+	var text = name.contents().filter(function() {
+  		return this.nodeType == 3;
+	}).text();
+	alert("FILTER BY: " + text);
+	// console.log(name);
 	// add physics to items
 	// move relevant items up to top using animation
 });
+
+$(".circle").hover(
+	function () {
+		$(".circle-overlay").css("opacity", "0.7");
+		console.log("ugh");
+	},
+	function () {
+		$(".circle-overlay").css("opacity", "0");
+	}
+);
+
+
 
 // because I'm working with the physics library created by Mr. Doob,
 // a lot of the javascript will be playing with it and figuring out what works
