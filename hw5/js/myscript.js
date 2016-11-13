b0d2363be30bfb33eac42eb5232d75904736b950
Michael James <thelivingpc@gmail.com>
2016-11-12 23:55:26 -0500

    basics

---
+++ hw5/js/myscript.js
@@ -0,0 +1,9 @@
+$(".cat").on("click", function() {
+	alert("Test");
+	// add physics to items
+	// move relevant items up to top using animation
+});
+
+// because I'm working with the physics library created by Mr. Doob,
+// a lot of the javascript will be playing with it and figuring out what works
+// so unfortunately it's hard for me to put much pseudo-code here
