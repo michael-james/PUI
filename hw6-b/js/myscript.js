f69f1d2357c5f9522ddb7e51817e2857a886f8ed
Michael James <thelivingpc@gmail.com>
2016-12-04 00:34:43 -0500

    done! submitted

--- hw6-b/js/myscript.js
+++ hw6-b/js/myscript.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
+  // ################################################
   // Add smooth scrolling to all links
   // borrowed from http://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+  // ################################################
+
   $("a").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
@@ -23,7 +26,10 @@ $(document).ready(function(){
     }
   });
 
-  // portrait
+  // ################################################
+  // portrait image swap
+  // ################################################
+
   $('#portrait-cont').hover(
     function() {
       $("#portrait").attr("src","assets/me2.jpg");
@@ -31,20 +37,29 @@ $(document).ready(function(){
       $("#portrait").attr("src","assets/me.jpg");
   });
 
+  // ################################################
+  // portfolio item interaction + animation
+  // ################################################
+
   var currentContRatio = 0;
 
+  // determine aspect ratio of container
   function setWorkRatio () {
     var imgContainer = $(".work-item-pad");
     var contWidth = $(imgContainer).width(); // Max width for the image
     var contHeight = $(imgContainer).height(); // Max height for the image
     var contRatio = contWidth / contHeight; // Used for cont aspect ratio
+    
+    // if aspect ratio has changed, resize all images
     if (contRatio != currentContRatio) {
       sizeAll();
       currentContRatio = contRatio;
     }
   }
 
+  // update scale & position of image
   function sizeWork(img, scale, anim) {
+    // determine aspect ratio of container
     var cont = img.parent();
     var contWidth = $(cont).width(); // Max width for the image
     var contWidthScl = contWidth * scale;
@@ -74,13 +89,14 @@ $(document).ready(function(){
     if (isNaN(x)) {x = 0;}
     if (isNaN(y)) {y = 0;}
 
+    // determine location and size of vignette overlay
     var overlay = img.parent().find(".work-item-overlay");
-    // console.log(overlay);
     overlay.css("width", contWidth);
     overlay.css("height", contHeight);
     overlay.css("top", cont.position().top);
     overlay.css("left", cont.position().left);
 
+    // determine image position & size based on container aspect ratio
     if (contRatio > imgRatio) {
       // image is stouter than container 
       var newWidth = contWidthScl;
@@ -88,6 +104,7 @@ $(document).ready(function(){
       var offsetY = ((newHeight) - contHeight) / 2 - y;
       var offsetX = ((newWidth) - contWidth) / 2 - x;
 
+      // animate to new position and size
       $(img).animate({
         width: contWidthScl,
         height: contWidthScl / imgRatio,
@@ -101,6 +118,7 @@ $(document).ready(function(){
       var offsetY = ((newHeight) - contHeight) / 2 - y;
       var offsetX = ((newWidth) - contWidth) / 2 - x;
 
+      // animate to new position and size
       $(img).animate({
         width: contHeightScl * imgRatio,
         height: contHeightScl,
@@ -110,13 +128,14 @@ $(document).ready(function(){
     }
   };
 
+  // resize and position all portfolio images
   function sizeAll() {
     $('.work-item-pad img').each(function() {
       sizeWork($(this), 1, false);
-      // console.log("size!");
     });
   };
 
+  // event listener for portfolio items, sets desired scale
   $(".work-item-pad").hover(function() {
       //mouse in
       sizeWork($(this).find("img"), 1.1, true);
@@ -125,6 +144,8 @@ $(document).ready(function(){
       sizeWork($(this).find("img"), 1, true);
   });
 
+  // determine if container aspect ratios have changed
+  // if so, resize all images
   $(window).resize(function() {
     setWorkRatio();
   });
@@ -133,29 +154,34 @@ $(document).ready(function(){
     setWorkRatio();
   })
 
-  // borrowed from bootstrap
+  // ################################################
   // modal 
+  // modified from bootstrap
+  // ################################################
+
   $('#modal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget); // Button that triggered the modal
     var recipient = button.data('whatever');// Extract info from data-* attributes
     var image = button.find('img');
-    image.css("width", "");
-    image.css("height", "");
-    image.css("display", "");
-    image.css("top", "");
-    image.css("left", "");
-    image.css("overflow", "");
+    var src = image.attr("src");
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    modal.find('.modal-title').text(recipient)
-    modal.find('.modal-body #imghere').contents().remove();
-    image.clone().prependTo(modal.find('.modal-body #imghere'));
+    var modal = $(this);
+    modal.find('.modal-title').text(recipient);
+    var imgDiv = modal.find('.modal-body #imghere');
+    imgDiv.css("background-image", "url(" + src + ")");
   })
 });
 
+// ################################################
+// initialize WOW package for scroll animations
+// ################################################
 new WOW().init();
 
+// ################################################
+// determine whether to show/hide the header
+// modified from http://www.codrops.com
+// ################################################
 /**
  * cbpAnimatedHeader.js v1.0.0
  * http://www.codrops.com
@@ -170,6 +196,8 @@ var cbpAnimatedHeader = (function() {
 
   var docElem = document.documentElement,
     header = document.querySelector( '.navbar-fixed-top' ),
+    navCollapse = document.getElementById( 'bs-example-navbar-collapse-1'),
+    navCollapseBtn = document.getElementById( 'btn-collapse'),
     didScroll = false,
     elemTop = $("#intro-thresh").offset().top;
     changeHeaderOn = elemTop;
@@ -190,6 +218,9 @@ var cbpAnimatedHeader = (function() {
     }
     else {
       header.classList.add('navbar-hidden');
+      navCollapse.classList.remove('in');
+      // navCollapse.classList.add('collapsing');
+      // navCollapseBtn.classList.remove('collapsed');
     }
     didScroll = false;
   }
@@ -200,4 +231,4 @@ var cbpAnimatedHeader = (function() {
 
   init();
 
-})();
+})()
