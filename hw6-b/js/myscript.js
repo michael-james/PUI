70f0b1fcffe53740804871e03f8778028f8da821
Michael James <thelivingpc@gmail.com>
2016-12-03 22:44:04 -0500

    modal image edit

--- hw6-b/js/myscript.js
+++ hw6-b/js/myscript.js
@@ -141,6 +141,10 @@ $(document).ready(function(){
     var image = button.find('img');
     image.css("width", "");
     image.css("height", "");
+    image.css("display", "");
+    image.css("top", "");
+    image.css("left", "");
+    image.css("overflow", "");
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this)
